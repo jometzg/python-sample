d46ac98c986cdf7d3dc55659f21c90f360fb7cce
John Metzger <56820659+jometzg@users.noreply.github.com>
2024-01-26 11:46:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# samnple flask app. One route to return a name from the environment variables
+# and another route to return the body of a POST request as JSON, delegated to
+# a second remote API endpoint.
+
 import os
 import requests
 from flask import Flask, jsonify, request
@@ -8,29 +12,29 @@ app = Flask(__name__)
 # Load environment variables from .env file
 load_dotenv()
 
-# Rest of your code...
+# home route
 @app.route('/') 
 def home(): 
   return "Hello, World!" 
 
 @app.route('/api/data', methods=['GET']) 
 def get_data(): 
-  # Replace with your data fetching logic 
+  # fetch a Name from the environment 
   data = {"key": os.getenv("NAME")} 
   return jsonify(data) 
 
 @app.route('/api/data', methods=['POST']) 
 def post_data(): 
-  # Replace with your data processing logic 
+  # return what was sent body as JSON 
   data = request.get_json() 
   return jsonify(data), 201 
 
 @app.route('/api/todo', methods=['POST']) 
 def post_todo(): 
-    # Replace with your data processing logic 
+    # get the body of the request 
     data = request.get_json() 
 
-    # Make an HTTP POST request to the desired endpoint
+    # Make an HTTP POST request to the "URL" endpoint
     response = requests.post(os.getenv("URL"), json=data)
 
     # Return the response as JSON with status code 201
